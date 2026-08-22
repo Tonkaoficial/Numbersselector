7723fb9a9971feddd4d0e850ad30eeaab7e5fb64
Tonkaoficial <alcpowerbi@gmail.com>
2026-08-22 18:56:18 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'selector-numero-v1';
+const CACHE = 'selector-numero-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
